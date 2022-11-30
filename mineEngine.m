c23dd9c6e7d007b00533842e322a5f19b14f1db9
erthiccy <115375341+erthiccy@users.noreply.github.com>
2022-11-29 22:44:57 -0800

    Update mineEngine.m
    Starting screen

--- mineEngine.m
+++ mineEngine.m
@@ -1,71 +1,9 @@
-% todo
-% 1. work out the bomb indexing logic
-% 2. create the basic graphics with buttons and other interactables
-%       remember, when button is clicked, it goes invisible, then draw the
-%       number in the same space
-
-
-classdef mineEngine
-
-    properties
-        minefield
-        numfield
-        graphics
-        gamestate
-    end
-    
-    methods
-        
-        % init method
-        function obj = mineEngine(rows, cols, numMines)
-            
-            % create random array to make a logical minefield
-            ranNums = randperm(rows*cols, rows*cols);
-            tempMines = reshape(ranNums, rows, cols);
-
-            obj.minefield = tempMines <= numMines;
-
-            % create array with numbers of mines around square
-            obj.numfield = zeros(rows, cols);
-
-            % shift indexes
-            rShiftUp = 1:rows-1;
-            rShiftDown = 2:rows;
-            cShiftLeft = 1:cols-1;
-            cShiftRight = 2:cols;
-
-            % adding up the numfield
-            obj.numfield(rShiftUp,:) = obj.numfield(rShiftUp,:)+obj.minefield(rShiftDown,:);
-            obj.numfield(rShiftDown,:) = obj.numfield(rShiftDown,:)+obj.minefield(rShiftUp,:);
-            obj.numfield(:,cShiftLeft) = obj.numfield(:,cShiftLeft)+obj.minefield(:,cShiftRight);
-            obj.numfield(:,cShiftRight) = obj.numfield(:,cShiftRight)+obj.minefield(:,cShiftLeft);
-            obj.numfield(rShiftUp,cShiftLeft) = obj.numfield(rShiftUp,cShiftLeft)+obj.minefield(rShiftDown,cShiftRight);
-            obj.numfield(rShiftUp,cShiftRight) = obj.numfield(rShiftUp,cShiftRight)+obj.minefield(rShiftDown,cShiftLeft);
-            obj.numfield(rShiftDown,cShiftLeft) = obj.numfield(rShiftDown,cShiftLeft)+obj.minefield(rShiftUp,cShiftRight);
-            obj.numfield(rShiftDown,cShiftRight) = obj.numfield(rShiftDown,cShiftRight)+obj.minefield(rShiftUp,cShiftLeft);
-
-
-           % initate the game window
-           obj.graphics = figure('Name', 'group12 minesweeper', ...
-               'Visible', 'off');
-
-            % set the game state to start
-            obj.gamestate = 'start';
-
-        end
-
-        % check for mine
-        function [check,state] = checkBomb(obj, row,col)
-            check = obj.minefield(row,col);
-
-            % if they click on a bomb, gameover
-            if check
-                state = 'gameover';
-            else
-                state = obj.gamestate;
-            end
-        end
-        
-    end
+figure('Name','ElianisAgenius','NumberTitle','off')
+row = 10;
+col = 10;
+for i = 1:row
+for j = 1:col
+uicontrol('Style','Pushbutton','position',[10+j33,10+i33,35,35])
+end
 end
 
